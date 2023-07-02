8786c65876d7e4ba195fcda1506cbed8b6ff689f
Aday Reyes <aday@sellerpoint.com>
2023-07-02 15:59:10 +0200

    progress

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Message, Chat
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ["text", "author", "created_at", "receiver"]
+    fields = ["text", "author", "receiver", "chat"]
+    search_fields = ["text"]
+
 
 # Register your models here.
+
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat)
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    if request.method == "POST":
+        print(request.POST["textmessage"])
+    return render(request, "chat/index.html", {"username": "pepito"})
