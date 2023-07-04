67e52f52b88f03f821b66bb0a118575afc1532b7
Aday Reyes <aday@sellerpoint.com>
2023-07-04 23:19:25 +0200

    show messagesin html

--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from .models import Message
+from .models import Chat
 
 
 def index(request):
     if request.method == "POST":
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(
+            text=request.POST["textmessage"],
+            chat=myChat,
+            author=request.user,
+            receiver=request.user,
+        )
         print(request.POST["textmessage"])
-    return render(request, "chat/index.html", {"username": "pepito"})
+    chatMessages = Message.objects.filter(chat__id=1)
+    return render(request, "chat/index.html", {"messages": chatMessages})
